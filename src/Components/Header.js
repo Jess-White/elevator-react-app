f21bc0373c2515177f4efc5a22883f2eb2660ce3
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-07-10 15:15:11 -0500

    removed header buttons in progress

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Jumbotron} from 'react-bootstrap';
 import {Container} from 'react-bootstrap';
-import NavLink from "react-bootstrap/NavLink";
+// import NavLink from "react-bootstrap/NavLink";
 
 function Header() {
   return (
