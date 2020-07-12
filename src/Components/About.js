bfb82fe646f7a8390576035efd424d2b622a1a19
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-07-12 14:47:55 -0500

    added header buttons

--- src/Components/About.js
+++ src/Components/About.js
@@ -27,9 +27,6 @@ class About extends Component {
         }))
       .then(response => 
         console.log(this.state.APOData))
-      .then(
-        response => 
-        console.log(this.state.NebularData))
         };
 
   render() {
@@ -43,14 +40,33 @@ class About extends Component {
     return (
       <div className="container">
         <div>
-          <h1>About This Elevator</h1>
-          <p>I started working on this project when I was reviewing third-party APIs in React. I had created a Frankenapp and Github repo to play around with third-party API calls, using several from this list of third-party APIs. 
+          <h1>About Ascension</h1>
+          <p>
 
-            One of the APIs I found was this one: 
+          This project was created in response to a technical challenge: "Build a virtual elevator." We were encouraged to be creative, and to build a fully-realized project, so I decided to create a React app with the floors as React components. When I received this challenge, I was reviewing third-party API requests in React, so I decided to integrate that work into my project.
 
-            which pulls data from NASA's APOD (Astronomy Photo Of the Day) website. You can see a sample image, the most recent, below. I decided to use the nebula images for this project, since they look cool, and since they would be a fun way to add visual interest and explore presentation and container elements in React.
+          I started by digging around in this list of third-party APIs, and decided to work with this one, which was built as a supplement for NASA's Astronomy Photo of the Day (APOD) API, since the programmer wasn't satisfied with NASA's setup. The photographs are gorgeous, but also because it's extremely well-organized and consistent and includes a lot of query options for playing around with data. 
 
-              My second-favorite third-party API is this one, which shows you random pictures of foxes. I've been trying to come up with an organic way to integrate it into a project for a while, but nothing so far. If you have a use for a random fox, I recommend it!</p>
+          I decided that I wanted to figure out how to render images from the API as "floors" in the elevator, so that when you clicked on a button, you'd be routed to a different floor. The "buttons" would be routes in a navigation bar, and the floors would be components set up as routes using React router. 
+
+          First I built my data component with the API call, mapping the results into an array similar to the one below. 
+
+          Then I built my floor components, along with a main page and header element. 
+
+          Then I set up React router and built a navigation bar for the router links. 
+
+          Then I spent like two days making an elevator door component and some super sweet glowy buttons. I used this free background image site to make everything look like the set of Star Trek: Insurrection, the best Star Trek movie and the non pareil of space opera art design. 
+
+          Finally, it was time to program in the core functionality of the app! 
+
+          This is what the APOD API data looks like:
+
+
+          I added logic in the router that passes in an array index (0-9) for each Navlink in the Navigation bar, so that when you click on a "button," you're routed to a "Floor" that pulls data from a corresponding object in the data array. The "url" value in each object is rendered as the background image source for the Elevator Door component's Sliding Door Element background panel. 
+
+          The Basement, Lobby, Penthouse, and Rooftop floors are hard-coded with images outside of the array, and the About page is a separate link altogether. 
+
+          </p>
 
           <h1>First APOD API Call: "Most Recent"</h1>
           <h1>{this.state.APOData.title}</h1>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import {Jumbotron} from 'react-bootstrap';
 import {Container} from 'react-bootstrap';
-// import NavLink from "react-bootstrap/NavLink";
+import NavLink from "react-bootstrap/NavLink";
 
 function Header() {
   return (
     <header>
       <Jumbotron fluid className="header">
+      <div className="row">
       <div className="panel">
               <Container fluid>
-                <div className="row logo">
+                <div className="column align-left logo">
                   <img src="/Ascension_logo.png" alt=""/>
                 </div>
 
                 
               </Container>
+      </div>
+
+      <div className="column header-buttons">
+      <NavLink onClick={() => this.props.handleFloorChange(2)} className="navbar-brand-top" to="/floor-two">2</NavLink>
+      <NavLink onClick={() => this.props.handleFloorChange(2)} className="navbar-brand-top" to="/floor-two">2</NavLink>
+      <NavLink onClick={() => this.props.handleFloorChange(2)} className="navbar-brand-top" to="/floor-two">2</NavLink>
+      </div>
       </div>
             </Jumbotron>
     </header>
