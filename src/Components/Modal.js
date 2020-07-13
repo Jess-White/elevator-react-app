3feddd7ea90abee4881e803dd32e9addb8fe4f44
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-07-13 13:24:24 -0500

    finalized

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -13,7 +13,7 @@ function Modal() {
         <a class="close" href="/">x</a>
         <div>
         <h1>About Ascension</h1>
-        <h3>This app is my answer to the Virtual Elevator technical challenge. I created a React app with the floors as React components. I started by digging around in this list of third-party APIs, and decided to work with <a href="https://apodapi.herokuapp.com">this one</a>, which was built as a supplement for NASA's Astronomy Photo of the Day (APOD) API. I used React router and CSS styling to turn the navigation bar into an elevator panel that routes to all "floor" components. The images are pulled in from the API and then rendered as a background image in the Elevator Door component, along with a function that yokes each value in the data array to a given floor. Click on the elevator buttons to see how it works!</h3>
+        <h3>This app is my answer to the Virtual Elevator technical challenge. I created a React app with the floors as React components. I started by digging around in <a href="https://github.com/public-apis/public-apis">this list</a> of public APIs, and decided to work with <a href="https://apodapi.herokuapp.com">this one</a>, which was built as a supplement for NASA's Astronomy Photo of the Day (APOD) API. I used React router and CSS styling to turn the navigation bar into an elevator panel that routes to all "floor" components. The images are pulled in from the API and then rendered as a background image in the Elevator Door component, along with a function that yokes each value in the data array to a given floor. Click on the elevator buttons to see how it works!</h3>
       </div>
 
      </div>
